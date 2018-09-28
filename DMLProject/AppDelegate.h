48a1732d961d6f21672ff8f2e140241796be460e
chenhaifeng <1397760231@qq.com>
2018-09-28 17:42:41 +0800

    <注释>

---
+++ DMLProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DMLProject
+//
+//  Created by chenhaifeng on 2018/9/28.
+//  Copyright © 2018年 geely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
